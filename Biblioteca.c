2142b78a9f377d0e3f7023b2d7448f8c45eac6db
Mariana <92758993+Got7mj@users.noreply.github.com>
2024-10-21 09:33:58 -0300

    Projeto 0.5

---
+++ Biblioteca.c
@@ -0,0 +1,14 @@
+#include <time.h>
+#include <stdlib.h>
+
+void delay(int segundos) {
+  int tempo = 1000 * segundos;
+  clock_t inicio = clock();
+  while (clock() < inicio + tempo) {
+    // não faz nada, apenas gasta tempo
+  }
+}
+void limpaTela(void) {
+  if (system("clear") || system("cls")) {
+  }
+}
